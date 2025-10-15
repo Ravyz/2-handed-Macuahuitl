84cbc7a147fee42915117e0f73e9167fbcd75571
Jonathan Gregg <147885457+Ravyz@users.noreply.github.com>
2025-10-15 00:16:31 -0400

    fix

--- 2-handed-macuahuitl1.21.1/src/client/java/ravyz/mod/mixin/client/PlayerEntityRendererMixin.java
+++ 2-handed-macuahuitl1.21.1/src/client/java/ravyz/mod/mixin/client/PlayerEntityRendererMixin.java
@@ -1,24 +1,23 @@
 package ravyz.mod.mixin.client;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntityRenderer.class)
-public class TwoHandedMacuahuitl {
+public class PlayerEntityRendererMixin {
 	@Inject(method = "getArmPose", at = @At("HEAD"), cancellable = true)
 	private static void TwoHandedMacuahuitl$getArmPoseDR(AbstractClientPlayerEntity player, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir) {
 		ItemStack itemStack = player.getMainHandStack();
-        if (itemStack.getName().equals(Text.literal("Ravyz Blade"))) {
+        if (itemStack.getName().equals(Text.literal("Ravyz Blade")) && itemStack.getItem() instanceof SwordItem) {
             cir.setReturnValue(BipedEntityModel.ArmPose.CROSSBOW_HOLD);
         }
         }}
